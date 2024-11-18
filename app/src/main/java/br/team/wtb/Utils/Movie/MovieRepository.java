7affb2a7efd284dd93fb7199d86025fa397e5051
Duque <devgduque@gmail.com>
2024-11-18 11:58:52 -0300

    Adiciona filmes mockados

--- app/src/main/java/br/team/wtb/Utils/Movie/MovieRepository.java
+++ app/src/main/java/br/team/wtb/Utils/Movie/MovieRepository.java
@@ -17,7 +17,10 @@ public class MovieRepository {
         movieList = new ArrayList<>();
         // Adiciona filmes de exemplo ou busca de um banco de dados/API
         movieList.add(new Movie(R.drawable.img_poster_whiplash, "Whiplash", 2014, 5, false, "https://www.youtube.com/watch?v=iTgk3WbTErk&pp=ygUQd2hpcGxhc2ggdHJhaWxlcg%3D%3D"));
-        movieList.add(new Movie(R.drawable.img_poster_whiplash, "Whiplash", 2014, 5, false, "https://www.youtube.com/watch?v=iTgk3WbTErk&pp=ygUQd2hpcGxhc2ggdHJhaWxlcg%3D%3D"));
+        movieList.add(new Movie(R.drawable.img_poster_ray, "Ray", 2004, 4, true, "https://www.youtube.com/watch?v=jVHCQfcugdw&pp=ygULcmF5IHRyYWlsZXI%3D"));
+        movieList.add(new Movie(R.drawable.img_poster_once, "Once", 2008, 4, false, "https://www.youtube.com/watch?v=K4uFFNl6FQ4&pp=ygUMb25jZSB0cmFpbGVy"));
+        movieList.add(new Movie(R.drawable.img_poster_begin, "Begin Again", 2014, 4, true, "https://www.youtube.com/watch?v=uTRCxOE7Xzc&pp=ygUTYmVnaW4gYWdhaW4gdHJhaWxlcg%3D%3D"));
+        movieList.add(new Movie(R.drawable.img_poster_bohemian, "Bohemian Rhapsody", 2018, 5, false, "https://www.youtube.com/watch?v=GryRsVhOvxo&pp=ygUQYm9oZW1pYW4gdHJhaWxlcg%3D%3D"));
     }
 
     public static synchronized MovieRepository getInstance() {
@@ -29,7 +32,18 @@ public class MovieRepository {
 
     // Método para retornar a lista de filmes
     public List<Movie> getMovies() {
-        return movieList;
+        List<Movie> sortedMovies = new ArrayList<>(movieList); // Cria uma cópia para preservar a lista original
+        sortedMovies.sort((movie1, movie2) -> {
+            // Ordena favoritos primeiro
+            if (movie1.getFavorite() && !movie2.getFavorite()) {
+                return -1; // Favorito antes de não favorito
+            } else if (!movie1.getFavorite() && movie2.getFavorite()) {
+                return 1; // Não favorito depois
+            }
+            // Ordena alfabeticamente pelo título
+            return movie1.getMovieName().compareToIgnoreCase(movie2.getMovieName());
+        });
+        return sortedMovies;
     }
 
     // Método para retornar uma lista de filmes que são favoritos
